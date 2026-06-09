908c2a48dde64c089c5370eb685fbf2029b93464
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2026-06-09 10:13:42 -0300

    Modulo E
    Repeticoes
    While
    Do while
    For

--- estudos/AULA13/ambiente.js
+++ estudos/AULA13/ambiente.js
@@ -1,20 +1,24 @@
+/*
 var c = 1
 do {
-    console.log(`Passo ${c}`)
-    c++
+  console.log(`Passo ${c}`)
+  c++
 } while (c <= 6)
-
-/*var c = 1 
-while ( c <= 6) {
-    console.log(`Passo ${c}`)
-    c++
-}
 */
 
+/* Essa e a mais encontrada dentro de programas */
+var c = 1
+while (c <= 500) {
+  console.log(`Passo ${c}`)
+  c++
+}
+
+
 /*
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
-console.log('tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
 */
--- estudos/AULA14/ambiente.js
+++ estudos/AULA14/ambiente.js
@@ -1,5 +1,13 @@
-console.log('Vai começar...')
-for(var c = 1; c <= 5;c++){
-    console.log(c)
+/*
+var c = 1
+while (c <= 15) {
+  console.log(`Passo ${c}`)
+  c++
 }
-console.log('FIM!')
+*/
+
+console.log('Vai comecar...')
+for (var c = 1; c <= 15; c++) {
+ console.log(`Contador ${c}`)
+} 
+console.log('FIM')
