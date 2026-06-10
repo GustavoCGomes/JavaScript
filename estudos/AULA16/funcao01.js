fa1e780ac8ed20b5af92ce52d157df075be49f7c
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2026-06-09 22:40:48 -0300

    Funcoes

--- estudos/AULA16/funcao01.js
+++ estudos/AULA16/funcao01.js
@@ -1,12 +1,12 @@
 function parimpar (n) {
-    if (n%2 == 0) {
-        return 'Par!'
-    } else {
-        return 'Ímpar!'
-    }
+  if (n % 2 == 0) {
+    return 'Par!'
+  } else {
+     return 'Ímpar!'
+  }
 }
 
-let res = parimpar(25)
-console.log(res)
+let res = parimpar(23)
+console.log(`O numero é ${res}`)
 
 //console.log(parimpar(24))
--- estudos/AULA16/funcao05.js
+++ estudos/AULA16/funcao05.js
@@ -1,13 +1,13 @@
 //RECURSIVIDADE
 function fatorial (n) {
-    if ( n == 1 ) {
-        return 1
-    } else {
-        return n * fatorial(n-1)
-    }
+  if ( n == 1 ) {
+    return 1
+  } else {
+    return n * fatorial(n-1)
+  }
 }
 
-console.log(fatorial(5))
+console.log(fatorial(4))
 
 
 
