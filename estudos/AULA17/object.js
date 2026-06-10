7cff5d932083a63fe827714168bad21676bb359d
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2026-06-09 22:55:21 -0300

    Update Object

--- estudos/AULA17/object.js
+++ estudos/AULA17/object.js
@@ -1,9 +1,13 @@
-let amigo = { nome: 'José',
-sexo: 'M',
-peso: 85.4,
-engordar(p=0){
+let amigo = { 
+  nome: 'José',
+  sexo: 'M',
+  peso: 85.4,
+
+  engordar(p = 0){
     console.log('Engordou')
     this.peso += p
-}}
+  } 
+}
+
 amigo.engordar(2)
 console.log(`${amigo.nome} peso ${amigo.peso}Kg`)
