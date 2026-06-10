2ee77dae7e283e9d160d67074b7d08b29b81b3c4
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2026-06-09 21:43:26 -0300

    Modulo F - Estudos Avancado em JS
    Variaveis Compostas
    array vetor

--- estudos/AULA15/ambiente.js
+++ estudos/AULA15/ambiente.js
@@ -1,10 +1,10 @@
-let num = [5,8,9,2,3]
+let num = [5,8,4,2,3] /*Variavel Composta*/
 num.push(1)
 num.sort()
 console.log(num)
 console.log(`Nosso vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-let pos = num.indexOf(8)
+let pos = num.indexOf(7)
 if(pos == -1){
     console.log('O valor não foi emcontrado!')
 } else {
